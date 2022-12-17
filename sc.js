da17575558d2b6728990a53208460ba0230277c2
aliabar <114748399+aliabar@users.noreply.github.com>
2022-12-17 02:31:46 -0800

    first commit

---
+++ sc.js
@@ -0,0 +1,16 @@
+document.getElementById("nem").innerHTML="Ali abdo"
+document.getElementById("pr").innerHTML="Front developer"
+document.getElementById("h33").innerHTML="HI THER !"
+
+var a=30;
+var b=20;
+var c=70;
+var d=90;
+var e=20;
+var f=70;
+var g=50;
+var k=30;
+var l=60;
+var m=70;
+var q=(a+b+c+d+e+f+g+k+l+m)/10;
+console.log(q)
